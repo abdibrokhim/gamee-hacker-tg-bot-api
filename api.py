29fc9efc02009f32791df984ac214499f881a131
abdibrokhim <abdibrokhim@gmail.com>
2024-04-06 14:05:13 +0500

    chore: update

--- api.py
+++ api.py
@@ -120,5 +120,5 @@ def signup(username: str, email: str) -> dict:
     return {"status": 400, "message": "Either username or email is missing"}
     
 
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8080)
